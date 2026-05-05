fb6cdac14fd89e4a9f3a8276604b6cffa7db0560
Jacob Collard <jacob@jacobcollard.com>
2026-05-05 11:45:09 -0400

    Fixed variable name

--- manage.py
+++ manage.py
@@ -7,8 +7,6 @@ import sys
 def main():
     """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mathoscope.settings')
-    if current_dir not in sys.path:
-        sys.path.insert(0, current_dir)
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
